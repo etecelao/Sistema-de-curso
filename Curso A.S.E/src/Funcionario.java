993d08deebc50f797275fbfb10f7ed67ff72860f
Tonio <atomob71@gmail.com>
2022-10-26 19:01:26 -0300

    1.1

--- Curso A.S.E/src/Funcionario.java
+++ Curso A.S.E/src/Funcionario.java
@@ -1,4 +1,8 @@
 public abstract class Funcionario {
     public String nome;
     public double salario;
+
+    public Funcionario(String nome) {
+        this.nome = nome;
+    }
 }
---
+++ Curso A.S.E/src/Servente.java
@@ -0,0 +1,26 @@
+public class Servente extends Funcionario{
+    private int tempo_trabalho_anos;
+
+    public Servente(String nome, int tempo_trabalho_anos) {
+        super(nome);
+        this.tempo_trabalho_anos = tempo_trabalho_anos;
+    }
+
+    public void calcularSalarioMensal(){
+        switch(tempo_trabalho_anos){
+            case 1: this.salario = 1200.00;
+            break;
+            case 2: this.salario = 1400.00;
+            break;
+            case 3: this.salario = 1650.00;
+            break;
+            default : this.salario = 1800.00;
+            break; 
+        }
+    }
+
+    public void status(){
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Salário: "+this.salario);
+    }
+}
